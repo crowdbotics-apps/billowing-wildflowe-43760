678a07b54a67c3210932a2cc0ccd8f954a3bcde5
Crowdbotics <team@crowdbotics.com>
2023-10-04 22:47:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-wildflowe-43760",
+  url: "https://billowing-wildflowe-43760.botics.co",
+  api: "https://billowing-wildflowe-43760.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
